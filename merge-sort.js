a71d653ad1392221578427c74dc23359fe4af8a8
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-05-02 14:52:57 -0500

    done

--- merge-sort.js
+++ merge-sort.js
@@ -1,34 +1,44 @@
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
-
+ if (arr.length <= 1) {
+  return arr;
+ }
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  
+  const sortedLeft = mergeSort([...left]);
+  const sortedRight = mergeSort([...right])
 
-  // Divide the array in half
-
-  // Recursively sort the left half
-  // Recursively sort the right half
-
-  // Merge the halves together and return
-
+  return merge(sortedLeft, sortedRight);
 }
 
 
 // Takes in two sorted arrays and returns them merged into one
 function merge(arrA, arrB) {
-
+  const arr = [];
   // Create an empty return array
+  while (arrA.length && arrB.length) {
 
-  // Point to the first value of each array
-  // While there are still values in each array...
-    // Compare the first values of each array
-    // Add the smaller value to the return array
-    // Move the pointer to the next value in that array
+    if (arrA[0] < arrB[0]) {
 
-  // Return the return array
+      arr.push(arrA.shift());
 
+    } else if (arrB[0] < arrA[0]) {
+
+      arr.push(arrB.shift());
+
+    }
+
+  }
+  return [...arr, ...arrA, ...arrB];
 }
+
+
+
 
 module.exports = [merge, mergeSort];
 
